4c5c92ec0713f6280e1a6bbdf8fffb32eb6dc68b
João_Thomaz <joaovitor_rodriguesthomaz@hotmail.com>
2023-12-21 17:58:01 -0300

    third commit 21_12

--- Mentalista/mentalist.js
+++ Mentalista/mentalist.js
@@ -4,6 +4,7 @@ while (jogoCompleto < 1){
 	var numeroSecreto = parseInt(Math.random() * 1001);
 	var tentativa = 0;
 	var tentativaLimite = 8;
+   var derrota = 0
 
 	while (tentativa != tentativaLimite+1){
 		var chute = prompt("Digite um número de 0 a 1000!")
@@ -26,7 +27,16 @@ while (jogoCompleto < 1){
 	if (tentativa == tentativaLimite+1 && chute != numeroSecreto){
 		alert("Você extrapolou a quantidade de tentativas! O número correto era: " + numeroSecreto);
 		jogoCompleto --;
+      derrota ++;
 	}
+
+   if (chute == numeroSecreto || derrota == 1) {
+      var resposta = confirm("Deseja continuar? \n\n1. Continuar\n2. Sair");
+
+      if (!resposta){
+         break;
+      }
+   }
 
 	jogoCompleto ++;
 	
